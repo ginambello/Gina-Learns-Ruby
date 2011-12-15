72611f5f9f056a4cb09d387536cd002ebece5fa0
Gina Bello <gina.m.bello@gmail.com>
2011-12-14 16:48:21 -0800

    first commit

---
+++ ex2.rb
@@ -0,0 +1,19 @@
+# Using a comment; this is so you can read your program later.
+# Anything after the # is ignored by Ruby.
+
+puts "I could have code like this." # and the comment after is ignored.
+
+# You can also use a comment to "disable" or comment out a piece of code:
+
+puts "This is a practice excercise;"
+print "this will run."
+
+# puts "This will not run."
+
+print "This was completed by: " 
+puts "Gina M Bello"
+
+# I learned that "puts" prints on a new line. And "print" prints. 
+
+# touch updates the time it was updated- date modified, and if there is no file then it makes a blank new one with the indicated name that you put
+
