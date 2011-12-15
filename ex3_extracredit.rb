952e1b8714dd082a473250b8e18f0bd9e355eaab
Gina Bello <gina.m.bello@gmail.com>
2011-12-15 15:37:09 -0800

    first commit

---
+++ ex3_extracredit.rb
@@ -0,0 +1,35 @@
+# This is my extra credit assignment 
+# I was assigned to calculate something
+# Here goes ...
+
+puts "I am calculating the amount of money I am going to spend for graduate school."
+
+puts  "I have a limit of about:", 
+      "                       100,000 dollars"
+
+puts "If tuition is '$49,900' per year and and there are 2.5 years, that means I am looking at spending:"
+puts 49900.00 * 2.50
+
+puts "And that is tuition only! There are still housing, transportation, books, and school supplies costs!"
+
+puts "Now let's say my parents can afford to give me 2,000 per academic year."
+puts 2000.00 * 2.50
+
+puts "So that's"
+puts 49900.00 * 2.50 - 2000.00 * 2.50
+puts "That what I will have to pay total for tuition"
+
+puts "If I find an apartment for '$1,100' per month,"
+puts "2.5 years is "
+puts 1100.000 * 2.500 * 12.000 
+puts "dollars of rent alone."
+
+puts "Let's add transportation costs of '$2,000' per year."
+puts 2000.00*2.50
+puts "And add '$2,000' per year for books."
+puts 2000.00*2.50
+puts "Lastly, add school supply costs of '$1,000' per year."
+puts 1000.00*2.50
+
+puts "So that's an over total of:"
+puts 49900.00 * 2.50 - 2000.00* 2 + 1100.00 * 2.50 * 12.00 + 2.00 * 2000.00 * 2.50 + 1000.00 * 2.50
