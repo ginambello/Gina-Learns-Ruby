a300ce526ef7f0b51ded2d79dcbe34f538e0b822
Gina Bello <gina.m.bello@gmail.com>
2011-12-15 14:13:58 -0800

    second commit

--- ex2.rb
+++ ex2.rb
@@ -17,3 +17,27 @@ puts "Gina M Bello"
 
 # touch updates the time it was updated- date modified, and if there is no file then it makes a blank new one with the indicated name that you put
 
+# git status tells you what you are doing
+# git log gives you log what you have committed
+# git add adds your file to
+
+# there's this thing called working file, the directory on your computer
+# touch ex3.rb adds ex3.rb
+# git add, adds to Index, is the cue to add to github
+# you have History and has history of all the commits in order
+# to get index stuff to history you do git commit
+# that moves it from index into history
+# working copy is physical copy
+# index is a cue
+# history is still on my computer
+# do ls -al to see history there is .git, this is a hidden file
+# on github all you have is another copy of the history
+# my computer to github requires a push
+# if you use more than one computer, Github can get ahead of you
+# let's say you were on another computer working on same doc, use gitpull
+# only push to your own stuff
+# fork it to get a copy of their stuff
+
+# control c stops the thing you typed, it aborts
+
+
