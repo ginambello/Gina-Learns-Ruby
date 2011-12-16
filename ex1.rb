22bc59ed3656d7da2d5ea893023d022ff8b2426a
Gina Bello <gina.m.bello@gmail.com>
2011-12-15 20:28:04 -0800

    Excercise 1

--- ex1.rb
+++ ex1.rb
@@ -2,9 +2,11 @@ puts "Hello World!"
 puts "Hello Again"
 puts "I like typing this."
 puts "This is fun."
+
 # puts 'Yay! Printing.'
 # puts "I'd much rather you 'not'."
 # puts 'I "said" do not touch this.'
+
 puts "This is my \"extra-credit\" assignment!"
 
 # The \ before tells Ruby that you intend it to print the following.
