688e6aff8cda00cc626c8588bfae9c450cd7bea9
Gina Bello <gina.m.bello@gmail.com>
2011-12-15 21:22:14 -0800

    Excercise 7

---
+++ ex7.rb
@@ -0,0 +1,34 @@
+puts "Mary had a little lamb."
+puts "Its fleece was white as %s." % 'snow'
+puts "And everywhere that Mary went"
+puts "." * 3 # The * 10 multiplies the string by 10 times. The string repeats.
+
+puts 
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# Below I use print, but what if you use put?
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 +end12
+puts
+puts end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 +end12
+
+# This is polite use of terminal
+
+puts 
+
+puts "Do you want %s with that?" % "fries"
+
+puts 
