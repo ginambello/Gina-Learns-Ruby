243dcefb8abbe883692ab980d9b2fb331ba7fd48
Gina Bello <gina.m.bello@gmail.com>
2011-12-15 20:37:07 -0800

    Excercise 3

--- ex3.rb
+++ ex3.rb
@@ -1,13 +1,16 @@
+# This excercise will practice symbols
+# The comma moves things over to a next line
+
 puts "I will now count my chickens:"
 
 puts "Hens", 25.0 + 30.0 / 6.0
 puts "Rosters", 100.0 - 25.0 * 3.0  % 4.0
 
 # % is for remainders
-# for smaller numbers it has a set of answers that come out right
-# floating points represents fractions
-# floating point is a base 2 not base 10
-# banks can't use floating points
+# For smaller numbers it has a set of answers that come out right
+# Floating points represents fractions
+# Floating point is a base 2 not base 10
+# Banks can't use floating points
 
 puts "Now I will count the eggs:"
 
