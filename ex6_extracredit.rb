1d2e6135a8551b0fcb2bb1301327c3ed5c56db07
Gina Bello <gina.m.bello@gmail.com>
2011-12-15 21:04:01 -0800

    Excercise 6

---
+++ ex6_extracredit.rb
@@ -0,0 +1,27 @@
+# This is my Excercise 6 Extra Credit
+
+binary = "binary"
+do_not = "don't"
+a = 10
+
+x = "There are #{a} types of people."
+y = "Those who know #{binary} and those who #{do_not}."
+
+puts x
+puts y
+
+puts "I said: '#{x}'"
+puts "I also said: '#{y}'"
+
+hilarious = "No."
+joke_evaluation = "Isn't that joke funny?! #{hilarious}"
+
+puts joke_evaluation
+puts hilarious
+
+w = "This is the left side of ... "
+e = "                               a string with a right side."
+
+puts w + e
+
+puts "There are #{27} lines in this extra credit assignment."
