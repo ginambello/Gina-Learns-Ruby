e842e34959545f9b71d43cf2f1c467f5d6a7475d
Gina Bello <gina.m.bello@gmail.com>
2011-12-15 20:32:48 -0800

    Excercise 2

--- ex2.rb
+++ ex2.rb
@@ -1,4 +1,4 @@
-# Using a comment; this is so you can read your program later.
+# Use a hash to add a comment; this is so you can read your program later.
 # Anything after the # is ignored by Ruby.
 
 puts "I could have code like this." # and the comment after is ignored.
@@ -13,31 +13,31 @@ print "this will run."
 print "This was completed by: " 
 puts "Gina M Bello"
 
-# I learned that "puts" prints on a new line. And "print" prints. 
+# I learned that "puts" prints on a new line, and "print" prints. 
 
 # touch updates the time it was updated- date modified, and if there is no file then it makes a blank new one with the indicated name that you put
 
 # git status tells you what you are doing
 # git log gives you log what you have committed
-# git add adds your file to
+# git add adds your file to the index
 
-# there's this thing called working file, the directory on your computer
-# touch ex3.rb adds ex3.rb
-# git add, adds to Index, is the cue to add to github
-# you have History and has history of all the commits in order
-# to get index stuff to history you do git commit
-# that moves it from index into history
-# working copy is physical copy
-# index is a cue
-# history is still on my computer
-# do ls -al to see history there is .git, this is a hidden file
-# on github all you have is another copy of the history
-# my computer to github requires a push
-# if you use more than one computer, Github can get ahead of you
-# let's say you were on another computer working on same doc, use gitpull
-# only push to your own stuff
-# fork it to get a copy of their stuff
+# There's this thing called working file- the directory on your computer.
+# touch ex3.rb adds ex3.rb if there was no ex3.rb before
+# git add, adds it to Index
+# Index is the queue to add to github
+# You have History and that has the history of all the commits in order
+# To get index stuff to history you do: git commit
+# That moves it from Index into History
 
-# control c stops the thing you typed, it aborts
+# Do ls -al to see history 
+# You'll see there is .git, this is a hidden file
+# On github website all you have is another copy of the history
+# My computer to github requires a push
+# If you use more than one computer github can get ahead of you
+# Let's say you were on another computer working on same doc, use git pull to go top-down
+# Only push to your own stuff
+# Fork it to get a copy of their stuff
+
+# Control c stops the thing you typed, it aborts
 
 
